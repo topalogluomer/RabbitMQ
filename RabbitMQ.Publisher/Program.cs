82bf71e96145291fb4c5e8cc482f305dd99721b7
path7 <path7@PATH>
2022-04-09 22:34:53 +0300

    Add project files.

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace RabbitMQ.publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("	amqps://szxgbist:ivBmeATJumRHZuj5Jsxfg1QuXLLUUmgY@sparrow.rmq.cloudamqp.com/szxgbist");
+            using var connection =factory.CreateConnection();
+
+            var channel=connection.CreateModel();
+            //durable:true dedim cünkü restart atıldıgında silinsin istemiyorum
+            //exculasive:false diyorum true dersem sadece buradaki channeldan baglantı kabul eder
+            //autoDelete: false dedim son subscriber
+            channel.QueueDeclare("hello Queue",true, false,false);
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+             {
+                 //rabbitMQ ya dosyalarını bytes dizisine cevirerek gönderebilirsin
+                 string message = $"Message {x}";
+                 var messageBody = Encoding.UTF8.GetBytes(message);
+                 // default exchange olacak
+                 channel.BasicPublish(string.Empty, "hello Queue", null, messageBody);
+
+                 Console.WriteLine($"mesajınız gönderilmistir: {message} ");
+
+             });
+
+           
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ RabitMQ.subcriber/Program.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabitMQ.subcriber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("	amqps://szxgbist:ivBmeATJumRHZuj5Jsxfg1QuXLLUUmgY@sparrow.rmq.cloudamqp.com/szxgbist");
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            //subscriber bu kuyruk olusmadan ayaga kalkarsa hata alırız
+            //gecmis oldugumuz parametlerin aynı olduguna dikkat edin
+            //channel.QueueDeclare("hello Queue", true, false, false);
+
+            channel.BasicQos(0, 1, false);
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume("hello Queue",false,consumer);
+
+
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message=Encoding.UTF8.GetString(e.Body.ToArray());
+                Thread.Sleep(1500);
+                Console.WriteLine("Gelen mesaj:" + message);
+                
+                //rabbitmq haberdar ediyoruz.
+                channel.BasicAck(e.DeliveryTag, false);
+
+            };
+
+
+
+
+            Console.ReadLine();
+
+        }
+
+      
+    }
+}
